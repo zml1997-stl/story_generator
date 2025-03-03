34fea32cad9557383705188340627946ded0a9d7
zml1997-stl <str8twojunk@gmail.com>
2025-03-02 22:14:38 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
