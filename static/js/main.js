775fb29faafc090f853257f7f42a0b7857e34879
zml1997-stl <str8twojunk@gmail.com>
2025-03-02 22:57:30 -0600

    Create main.js

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+// static/js/main.js
+
+// Function to display an alert message
+function showAlert(message) {
+  alert(message);
+}
+
+// Example usage: Show an alert when the page loads
+window.onload = function() {
+  showAlert("Welcome to the Interactive Story Generator!");
+};
