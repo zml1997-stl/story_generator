9aa40113c9d2cb1fe3be4186a3e10288c7f7ccd0
zml1997-stl <str8twojunk@gmail.com>
2025-03-02 23:03:33 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///stories.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
